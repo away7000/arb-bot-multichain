848c564b63657d239114e299512f633d2a68f757
away7000 <83220445+away7000@users.noreply.github.com>
2026-03-31 14:52:04 +0700

    Delete src/fetcher/oneinch.ts

--- src/fetcher/oneinch.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export async function getQuote(chainId: number, from: string, to: string, amount: string) {
-  const url = `https://api.1inch.io/v5.0/${chainId}/quote`;
-  const res = await axios.get(url, {
-    params: { fromTokenAddress: from, toTokenAddress: to, amount }
-  });
-  return res.data;
-}
